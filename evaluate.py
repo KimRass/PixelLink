7d4a2063974ca4dfd9fc8c167e1d23c4241a9a2e
KimRass <rmx1000@yonsei.ac.kr>
2023-08-24 14:54:23 +0900

    Employ running loss

--- evaluate.py
+++ evaluate.py
@@ -1,11 +1,13 @@
 
 
 def get_pixel_iou(pixel_pred, pixel_gt):
+    # pixel_pred = torch.randn(1, 2, 1768, 1256)
+    # pixel_gt = torch.randn(1, 1768, 1256)
     pixel_pred = (pixel_pred[:, 1, ...] >= 0.5).long()
     pixel_pred = (pixel_pred == 1)
     pixel_gt = (pixel_gt == 1)
 
-    intersec = pixel_pred & pixel_gt
-    union = pixel_pred | pixel_gt
+    intersec = (pixel_pred & pixel_gt).sum()
+    union = (pixel_pred | pixel_gt).sum()
     iou = intersec / union
     return iou.item()

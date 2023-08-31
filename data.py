1f000a650c484c46e81a0a57b118dea72b2c13f3
KimRass <rmx1000@yonsei.ac.kr>
2023-08-31 12:51:56 +0900

    Parse txt files

--- data.py
+++ data.py
@@ -52,10 +52,13 @@ class MenuImageDataset(Dataset):
         bboxes = list()
         with open(txt_path, mode="r") as f:
             line = f.readline().strip().replace("\ufeff", "")
-            try:
-                l, t, r, b, text = line.split("ᴥ ")
-            except:
-                print(line)
+            splitted = line.split("ᴥ ")
+            if len(splitted) == 5:
+                l, t, r, b, text = splitted
+            elif len(splitted) == 4:
+                l, t, r, b = splitted
+            else:
+                pass
             l = round(float(l))
             t = round(float(t))
             r = round(float(r))
